bc997a372c239a0ffeb5b68e481f6d5f657650eb
Swastik Banerjee <31207633+swastikbanerjee07@users.noreply.github.com>
2018-08-12 22:56:35 +0530

    Update README
    The code for the bot in python

---
+++ lambda_management.py
@@ -0,0 +1,75 @@
+response = {
+    "dialogAction": {
+        "type": "Close",
+        "fulfillmentState": "Fulfilled",
+        "message": {
+            "contentType": "PlainText",
+            "content": "Thanks"
+        }
+    }
+}
+
+response_table = {
+    "science": "There are 17 courses I can suggest you related to Science: BE,BTech, BArch, BSc, BCA, MBBS, BPharma. Arts courses are also available to Science students: BA, PerformingArts, FineArts. BDes in Animation/Design, BA LLB, Hotel Management, Journalism/Mass Communication, Diploma in Education (DEd). Commerce course options are also open for Science students: BCom, BBA LLB..............................................Please type fully any one of the courses suggested above to know about the best colleges to pursue the degree.",
+    "commerce": "There are 11 courses I can suggest you related to Commerce: BCom, BBA LLB, BCA, Chartered Accountancy (CA). Besides the above Commerce related courses, a Commerce student has the option to pursue various courses open to students after Class 12th Arts, such as: BA, PerformingArts, FineArts, BDes in Animation/Design, Hotel Management, Journalism/Mass Communication, Diploma in Education (DEd)..............................................Please type fully any one of the courses suggested above to know about the best colleges to pursue the degree.",
+    "humanities": "There are 10 courses I can suggest you related to Humanities: BA, PerformingArts, FineArts, BDes in Animation/Design, BA LLB, Hotel Management, Journalism/Mass Communication, Diploma in Education (DEd), BCom, BBA LLB, BCA ..............................................Please type fully any one of the courses suggested above to know about the best colleges to pursue the degree.",
+    "btech": "Apart from the top IITs and NITs, some of the other top engineering colleges include BITS Pilani, Delhi Technological University, Indian School of Mines(Dhanbad), Jadavpur University, IIEST(Indian Institute of Engineering Science and Technology), College of Engineering(Pune), VIT, Manipal, SRM, Thapar Institute of Engineering and Technology, PSG College of Technology, Aligarh Muslim University, Anna University, BMS College of Engineering, Symbiosis Institute of Technology(Pune), University College of Engineering(Osmania University), Harcourt Butler Technical University, Ramaiah Institute of Technology, Amity School of Engineering.",
+    "bachelor of architecture(barch)": "A few of the best colleges of architecture in India are: Sir J J College of Architecture(Mumbai), IIT Roorkie, Chandigarh College of Architecture, IIT Kharagpur, NIT Tiruchirappalli, NIT Calicut, Sushant School of Art and Architecture(Gurgaon), Faculty of Architecture and Ekistics,JMI(Delhi), BIT Mesra, R.V. School of Architecture(RVCE,Bangalore), Faculty of Architecture-Manipal University, School of Planning and Architecture(New Delhi), Rizvi College of Architecture(Mumbai), CEPT University(Ahmedabad), Anna University(Chennai), Maulana Azad National Institute of Technology(Bhopal), Malviya National Institute of Technology(Jaipur), Visvesvaraya National Institute of Technology(Nagpur), M.S. Ramaiah Institute of Technology(Bangalore), Narsee Monjee Institute of Management Studies(Mumbai).",
+    "bsc": "Indian Institute of Science(IISc), IISER, NISER, Chennai Mathematical Institute(CMI), Indian Statistical Institute(ISI), Indian Institute of Technology(IIT) are the top-notch universities for basic science in India. Apart form these,there are also extremely good colleges like UMDAE-CBS, St. Stephen’s College(Delhi University), Loyola College(Madras University), St. Xavier’s College, Presidency University, Christ University(Bangalore), Miranda House(Delhi University), Hansraj College(Delhi University), Fergusson College, Madras Christian College, Hindu College(DU), Sri Venkateswara College(DU), Ramjas College(DU).",
+    "bca(it & software)": "Some of the best colleges to study BCA in IT and Software is IIFA LANCASTER Degree College (Bengaluru), Department of Computer Applications, SRM University (Chennai), Institute of Management Studies (Noida), Madras Christian College (Chennai), Symbiosis Institute of Computer Studies & Research (Pune),  Birla Institute of Technology (Jharkhand), Presidency College (Bengaluru), University College-Kurukshetra (Kurukshetra), Stella Maris College (TamilNadu), Xavier Institute of Computer Application (Ahmedabad).",
+    "mbbs": "The top colleges to study Medical are AIIMS(All India Institute of Medical Sciences)Delhi , Christian Medical College(CMC) Vellore, Armed Forces Medical College(AFMC) Pune, Kasturba Medical College(KMC) Mangalore, Maulana Azad Medical College(MAMC) New Delhi, Jawaharlal Institute of Post Graduate Medical Education and Research(JIPMER) Pondicherry, Lady Hardinge Medical College(LHMC) New Delhi, Madras Medical College(MMC) Chennai, Grant Medical College(GMC) Mumbai, King George’s Medical University(KGMU) Lucknow, Seth GS Medical College(GSMC) Mumbai, Institute of Medical Sciences(IMS) Varanasi, Government Medical College(GMC) Amritsar, Mahatma Gandhi Institute of Medical Sciences(MGIMS) Wardha, Lokmania Tilak Municipal Medical College(LTMMC) Mumbai, IPGMER Kolkata, BMCRI Bangalore, Christian Medical College(CMC) Ludhiana, Osmania Medical College(OMC) Hyderabad, B.J. Medical College(BJMC) Ahmedabad.",
+    "Bachelor of Pharmacy (BPharma)": "Some of the best colleges to study BPharma are University Institute of Pharmaceutical Sciences (Chandigarh), Institute of Chemical Technology(ICT) Mumbai, GITAM(Visakhapatnam), Manipal College of Pharmaceutical Sciences(MCOPS) Manipal, Lovely Professional University(LPU) Jalandhar, Bombay College of Pharmacy(BCP) Mumbai, JSS College of Pharmacy Ooty, IIT BHU, LM College of Pharmacy(LMCP) Ahmedabad, Birla Institute of Technology(BIT Mesra) Ranchi, GOA College of Pharmacy(Panji), Poona College of Pharmacy(Pune), Jamia Hamdard University(New Delhi).",  
+    "Bachelor in Arts":"The top art colleges in India are Lady Shri Ram College(LSR) for Women New Delhi, Loyola College(Chennai), Christ University(Bangalore), St. Xavier;s College(Mumbai), Miranda House (New Delhi), Hansraj College (Delhi), Shri Ram College of Commerce (Delhi), Hindu College (New Delhi), Ramjas College (Delhi), Madras Christian College (Chennai), Delhi College of Arts & Commerce (Delhi), Sri Venkateswara College (Delhi), Stella Maris College (Chennai), Kristu Jayanti College (Bangalore), Mount Carmel College (Bangalore), Kamala Nehru College for Women (Delhi), Indraprastha College for Women (Delhi), GGDSD College (Chandigarh), Gargi College (New Delhi), K.J Somaiya College of Arts and Commerce (Mumbai) , Government College for Girls (Chandigarh), Maharaja Agrasen College (Delhi), Ethiraj College for Women (Chennai), DAV College (Chandigarh), St. Xavier's College Ahmedabad, Mehr Chand Mahajan D. A. V. College for Women(MCM DAV College for Women) Chandigarh, Kirori Mal College (Delhi), St. Francis College for Women (Hyderabad), Mithibai College of Arts Mumbai , The Oxford College of Arts (Bangalore), Women's Christian College (Chennai), St. Teresa's College (Kochi), Sacred Heart College (Kochi), St. Ann's College for Women (Hyderabad), Ramakrisha Mission Vidyamandir Kolkatta(Belur), S.K. Somaiya college of Arts and Commerce (Mumbai), Sophia College for Women (Mumbai), Isabella Thoburn College (Lucknow), St. Mira's College for Girls (Pune), Maharani Laxmi Ammanni College for Women (Bangalore).",
+    "Fine/PerformingArts":"The best colleges for Performing and Fine Arts are: Viswa Bharati University, National School of Drama, Satyajit Ray Film and Television Institute(SRFTI), Film and Television Institute of India Pune, Barry John Acting Studio Mumbai, Whistling Woods International Mumbai, Delhi Film Institute, RK Film and Media Academy Delhi, Kalakshetra Foundation(Chennai), College of Art New Delhi, Faculty of Visual Arts BHU, Sir JJ College of Applied Art Mumbai, Faculty of Fine Arts Maharaja Sayajirao University of Baroda, Faculty of Fine Arts Jamia Millia Islamia, College of Fine Arts Trivandrum, Jawaharlal Nehru Architecture and Fine Arts University Hyderabad, Government College of Fine Arts Chennai, Department of Fine Arts Mahatma Gandhi Kashi Vidyapith Varanasi, College of Fine Arts Karnataka Chitrakala Parishath Bangalore.",
+    "bdes in animation/design": "According to sources, here’s a list of top animation colleges in India: DSK International Campus in Pune; School of Animation and Visual Effects in Kolkata; Birla Institute of Technology in Noida, Jaipur ; Industrial Design Centre, IIT Bombay In Mumbai........................................... A few of the best design colleges in India are: National Institute of Design(NID) Ahmedabad, National Institute of Fashion Technology(NIFT) Mumbai,  NIFT Hauz Khas(New Delhi), Pearl Academy(New Delhi), NID Gandhinagar, Symbiosis Institute of Design(Pune), Lovely Professional University(LPU) Jalandhar, Industrial Design Centre IIT Bombay, NIFT Hyderabad, NID Vijayawada, NIFT Kannur, NIFT Bangalore, NIFT Bhubaneswar.",    
+    "ba llb": "The top law colleges in India are : National Law School of India University, Bangalore; National Law University, New Delhi; Rajiv Gandhi School of Intellectual Property Law, Indian Institute of Technology, Kharagpur; NALSAR University of Law, Hyderabad; National Law University, Jodhpur; WB National University of Juridical Sciences, Kolkata; Jindal Global Law School, Sonipat ;  Gujarat National Law University, Gandhinagar; Faculty of Law, University of Delhi, Delhi; National Law Institute University, Bhopal; Rajiv Gandhi National University of Law, Patiala; National University of Advanced Legal Studies, Kochi; Symbiosis Law School, Pune; Aligarh Muslim University, Aligarh; Banaras Hindu University, Varanasi.",
+    "hotel management": "Top colleges for hotel management are: Institute of Hotel Management Catering Technology and Applied Nutrition(IHM) Mumbai, Institute of Hotel Management Catering and Nutrition(IHM) Pusa New Delhi, Lovely Professional University(LPU) Jalandhar, Institute of Hotel Management(IHM) Bangalore, UEI Global Rohini(New Delhi), IHM Kolkata, International Institute of Hotel Management(IIHM) Kolkata, IHM Chennai, Dr. Ambedkar Institute of Hotel Management Catering and Nutrition(IHM) Chandigarh, Oberoi Centre of Learning and Development(OCLD) New Delhi.",
+    "journalism/mass communication": "The top colleges to study Journalism and Mass Communication in India are: Indian Institute of Journalism and Mass media(Bangalore), Symbiosis Institute of Media and Communication(Pune), Lady Shri Ram College for Women(New Delhi), Christ College(Bangalore), School of Communication(Manipal), Delhi College of Arts and Commerce, Indraprastha College for Women(New Delhi), St. Xavier’s College(Mumbai and Kolkata), Kishinchand Chellaram College, Kamala Nehru College(New Delhi), Madras Christian College.",
+    "diploma in education (ded)": "Some of the colleges to pursue Diploma in Education are University of Mumbai, CMJ University, Bombay Teacher’s Training College(BTTC), BJB Autonomous College(BJBAC), International Institute of Education and Research(Jaipur), Jaipur National University(JNU), Gandhi Shikshan Bhavan’s Smt Surajba College of Education, Annie Besant College of Engineering and Management, Maniben Nanavati Women’s College.",  
+    "bcom": "The best colleges for BCom in India are Shri Ram College of Commerce(SRCC) New Delhi, Lady Shri Ram College for Women(LSR) New Delhi, IIFA Multimedia (Mumbai), Loyola College(Chennai), St. Xavier’s College(Mumbai), Christ University(Bangalore), Hindu College(New College), St. Joseph’s College of Commerce(SJCC) Bangalore, Narsee Monjee College of Commerce and Economics(Mumbai), St. Andrew’s College of Arts Science and Commerce(Mumbai), Symbiosis College of Arts and Commerce(Pune), Hans Raj College(HRC) New Delhi, Mount Carmel College(MCC) Bangalore, Stella Maris College(Chennai), Madras Christian College(Chennai), St. Xavier’s College(Kolkata).",
+    "chartered accountancy (ca)": "The best colleges for CA in India are: Academy of Commerce(New Delhi), Northern India Regional Council of ICAI, Nahata Professional Academy(Indore), Vidya Sagar Career Institute Ltd.(Jaipur),  Yeshas Academy(Bangalore), Bangalore branch of SIRC of ICAI, Chanakya Academy for Management and Professional Studies, J.K. Shah Classes(Mumbai), Agrawal Classes(Pune).",   
+}
+
+response_table["be"] = response_table["btech"]
+response_table["btech/be"] = response_table["btech"]
+response_table["b.tech"] = response_table["btech"]
+response_table["b.e"] = response_table["btech"]
+response_table["bachelor of architecture"] = response_table["bachelor of architecture(barch)"]
+response_table["barch"] = response_table["bachelor of architecture(barch)"]
+response_table["b.arch"] = response_table["bachelor of architecture(barch)"]
+response_table["b.sc"] = response_table["bsc"]
+response_table["bca"] = response_table["bca(it & software)"]
+response_table["m.b.b.s."] = response_table["mbbs"]
+response_table["bpharma"] = response_table["Bachelor of Pharmacy (BPharma)"]
+response_table["bpharm"] = response_table["Bachelor of Pharmacy (BPharma)"]
+response_table["b.pharm"] = response_table["Bachelor of Pharmacy (BPharma)"]
+response_table["b.pharma"] = response_table["Bachelor of Pharmacy (BPharma)"]
+response_table["ba"] = response_table["Bachelor in Arts"]
+response_table["arts"] = response_table["humanities"]
+response_table["performingarts"] = response_table["Fine/PerformingArts"]
+response_table["finearts"] = response_table["Fine/PerformingArts"]
+response_table["llb"] = response_table["ba llb"]
+response_table["bba llb"] = response_table["ba llb"]
+response_table["diploma in education"] = response_table["diploma in education (ded)"]
+response_table["ded"] = response_table["diploma in education (ded)"]
+response_table["b.com"] = response_table["bcom"]
+response_table["chartered accountancy"] = response_table["chartered accountancy (ca)"]
+response_table["ca"] = response_table["chartered accountancy (ca)"]
+
+
+
+
+
+
+
+
+
+
+def lambda_handler(event, context):
+    # print(event)
+    subject_name = event["currentIntent"]["slots"]["Stream"]
+    if subject_name.lower() in response_table:
+        response["dialogAction"]["message"]["content"] = response_table[subject_name.lower()]
+        return response
+    
+    
